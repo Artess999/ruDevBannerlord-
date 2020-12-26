afcfcfd51272e511310ec51ebde541119639bef6
Кононенко Артем Игоревич <AIKononenko@alfabank.ru>
2020-12-27 02:49:01 +0300

    translation

--- tools/index.js
+++ tools/index.js
@@ -14,14 +14,15 @@ const convertToJsOptions = {
 
 const convertToXmlOptions = {
     compact: true,
-    spaces: 4,
+    spaces: 2,
 }
 
 const folders = ['Native', 'SandBox', 'SandBoxCore', 'StoryMode'];
 
 folders.forEach((folder) => {
     const resultsPath = path.resolve(RESULTS_PATH);
-    const resultsFolderPath = path.resolve(RESULTS_PATH, folder);
+    const resultsAbsentsPath = path.resolve(RESULTS_PATH, 'absents');
+    const resultsRudimentsPath = path.resolve(RESULTS_PATH, 'rudiments');
 
     const translationFolderPath = path.resolve(TRANSLATION_PATH, folder);
     const sourceFolderPath = path.resolve(SOURCE_PATH, folder);
@@ -29,7 +30,6 @@ folders.forEach((folder) => {
     fs.readdirSync(translationFolderPath).forEach(file => {
         const translationFilePath = path.resolve(translationFolderPath, file);
         const sourceFilePath = path.resolve(sourceFolderPath, file);
-        const fileName = path.basename(file, '.xml');
 
         const translationXml = fs.readFileSync(translationFilePath, 'utf8');
         const translationJs = convert.xml2js(translationXml, convertToJsOptions);
@@ -67,17 +67,46 @@ folders.forEach((folder) => {
                 fs.mkdirSync(resultsPath);
             }
 
-            if (!fs.existsSync(resultsFolderPath)){
-                fs.mkdirSync(resultsFolderPath);
+            if (!fs.existsSync(resultsAbsentsPath)){
+                fs.mkdirSync(resultsAbsentsPath);
+            }
+
+            if (!fs.existsSync(resultsRudimentsPath)){
+                fs.mkdirSync(resultsRudimentsPath);
             }
 
             if (absents.length > 0) {
+                const resultsFolderPath = path.resolve(resultsAbsentsPath, folder);
+
+                if (!fs.existsSync(resultsFolderPath)){
+                    fs.mkdirSync(resultsFolderPath);
+                }
+
                 const absentsJs = cloneDeep(translationJs);
                 absentsJs.base[0].strings[0].string = absents;
 
                 const absentsXml = convert.js2xml(absentsJs, convertToXmlOptions);
 
-                fs.writeFile(path.resolve(RESULTS_PATH, folder, file), absentsXml, function (err,data) {
+                fs.writeFile(path.resolve(resultsFolderPath, file), absentsXml, function (err,data) {
+                    if (err) {
+                        return console.log(err);
+                    }
+                });
+            }
+
+            if (rudiments.length > 0) {
+                const resultsFolderPath = path.resolve(resultsRudimentsPath, folder);
+
+                if (!fs.existsSync(resultsFolderPath)){
+                    fs.mkdirSync(resultsFolderPath);
+                }
+
+                const rudimentsJs = cloneDeep(translationJs);
+                rudimentsJs.base[0].strings[0].string = rudiments;
+
+                const rudimentsXml = convert.js2xml(rudimentsJs, convertToXmlOptions);
+
+                fs.writeFile(path.resolve(resultsFolderPath, file), rudimentsXml, function (err,data) {
                     if (err) {
                         return console.log(err);
                     }
